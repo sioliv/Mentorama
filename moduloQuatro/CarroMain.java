821f766cc343581e9b163c9b44ebb74a776bc9c0
sioliv <silvinha.olivert@gmail.com>
2021-12-20 08:35:30 -0300

    Classe Main

--- moduloQuatro/CarroMain.java
+++ moduloQuatro/CarroMain.java
@@ -9,12 +9,13 @@ public class CarroMain {
         //Construindo o objeto através da palavra chave NEW
 
 
-        Carro carro = new Carro(4, "VM12345", 2021, "Gasolina");    
+        Carro carro = new Carro(4, "VM12345", 2021, "Gasolina");
     
         carro.setCor(Carro.CINZA);
         carro.setQuantidadeCalotas(4);
         carro.setQuantidadePneus(4);
         carro.setQuantidadeParafusosPneu(0);
+        carro.setarCondicionado(true);
         carro.imprimeValores();
         
 
